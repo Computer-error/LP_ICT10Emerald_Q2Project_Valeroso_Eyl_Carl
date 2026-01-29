02a3bf34e3d935799a3332fb45b9ac5635e28554
Computer-error <valeroso.er@obmontessori.edu.ph>
2026-01-29 21:28:16 +0800

    Update main.py
    Modified rankings

--- main.py
+++ main.py
@@ -15,10 +15,44 @@ def calculate_average(event):
 
     average = (score1 + score2 + score3 + score4 + score5 + score6 + score7) / 7
 
+    if average = 100:
+        rank = "S"
+    elif average = 99:
+        rank = "A+"
+    elif average >= 96:
+        rank = "A"
+    elif average >= 93:
+        rank = "A-"
+    elif average >= 90:
+        rank = "B+"
+    elif average >= 87:
+        rank = "B"
+    elif average >= 84:
+        rank = "B-"
+    elif average >= 81:
+        rank = "C+"
+    elif average >= 78:
+        rank = "C"
+    elif average >= 75:
+        rank = "C-"
+    elif average >= 72:
+        rank = "D+"
+    elif average >= 69:
+        rank = "D"
+    elif average >= 66:
+        rank = "D-"
+    elif average >= 63:
+        rank = "F+"
+    elif average >= 60:
+        rank = "F"
+    else:
+        rank = "F-"
+
     if average >= 75:
         result = "passed"
     else:
         result = "failed"
 
     document.getElementById("averageA").innerText = str(round(average,2))
-    document.getElementById("result").innerText = ( Lname+", "+Fname+", a student from grade "+lev+set+", has "+result+" this quarter with a grade of "+str(round(average,2))+"." )
+
+    document.getElementById("result").innerText = ( Lname+", "+Fname+", a student from grade "+lev+set+", has "+result+" this quarter with a grade of "+str(round(average,2))+"("+rank+")""." )
