c0f636b8acc9d739577b896bb74e3861f7829e86
Computer-error <valeroso.er@obmontessori.edu.ph>
2026-01-29 22:14:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def calculate_average(event):
 
     average = (score1 + score2 + score3 + score4 + score5 + score6 + score7) / 7
 
-    if average = 100:
+    if average == 100:
         rank = "S"
-    elif average = 99:
+    elif average == 99:
         rank = "A+"
     elif average >= 96:
         rank = "A"
@@ -58,4 +58,5 @@ def calculate_average(event):
     document.getElementById("averageA").innerText = str(round(average,2))
 
     document.getElementById("result").innerText = ( Lname+", "+Fname+", a student from grade "+lev+set+", has "+result+" this quarter with a grade of "+str(round(average,2))+"("+rank+")""." )
+
 
